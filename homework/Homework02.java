5d211d70b6e5a5eb1e2bcb91ae8f542b1ef62e09
Seowoo <5w31892p@gmail.com>
2022-12-22 21:29:21 +0900

    오답 || 잘못된 숫자 입력시 계속 입력, 정답일 경우 코드 종료

---
+++ homework/Homework02.java
@@ -0,0 +1,26 @@
+package com.sparta.hbdstudy.homework;
+
+import java.util.Scanner;
+
+public class Homework02 {
+    public static void main(String[] args) {
+        // 오답일 경우, 계속 문제 풀기
+        // 정답일 경우, 코드 종료
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("다음 중 프로그래밍 언어가 아닌것은?");
+        System.out.println("1. Javascript\n2. Java\n3. 망둥어\n4. HTML");
+        while (true) {
+            int answer = scanner.nextInt();
+            if (3 == answer) {
+                System.out.println("정답입니다.");
+                break;
+            } else if (1 == answer || 2 == answer || 4 == answer) {
+                System.out.println("오답입니다.");
+                System.out.println("다시 입력해주세요.");
+            } else {
+                System.out.println("올바른 숫자가 아닙니다.");
+                System.out.println("1~4까지의 숫자만 입력해주세요.");
+            }
+        }
+    }
+}
